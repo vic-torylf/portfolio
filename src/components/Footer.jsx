9023c45749dd6cb0ba5d1cfeaccba11dbd1006fc
vic-torylf <victoryipstu@gmail.com>
2021-10-18 00:11:13 +0800

    "update"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <footer>
-            <a href="https://codepen.io/vic-torylf" target="_blank" rel="noreferrer">
+            <a href="https://github.com/vic-torylf" target="_blank" rel="noreferrer">
                 GitHub
             </a>
-            <a href="https://github.com/vic-torylf" target="_blank" rel="noreferrer">
+            <a href="https://codepen.io/vic-torylf" target="_blank" rel="noreferrer">
                 CodePen
             </a>
         </footer>
